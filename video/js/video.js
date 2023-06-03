4cada734fe462887e28187f9aa3e7b5958ab0ac4
fastwalkergy <106460370+fastwalkergy@users.noreply.github.com>
2023-06-03 15:39:35 +0800

    C

--- video/js/video.js
+++ video/js/video.js
@@ -26,3 +26,28 @@ function playVideo() {
 player.on('ended', function() {
   player.play();
 });
+
+ //控制台输出
+ var styleTitle1 = `
+ font-size: 20px;
+ font-weight: 600;
+ color: rgb(244,167,89);
+ `
+ var styleTitle2 = `
+ font-size:150px;
+ color: rgb(244,167,89);
+ `
+ var styleContent = `
+ color: rgb(30,152,255);
+ `
+ var title1 = 'PSVD'
+ var title2 = `
+ PSVD                                               
+ `
+ var content = `
+ 版 本 号：1.2
+ 更新日期：2023-05-21
+ 
+ `
+ console.log(`%c${title1} %c${title2}
+ %c${content}`, styleTitle1, styleTitle2, styleContent)
